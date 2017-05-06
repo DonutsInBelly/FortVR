97c546454fbf9d06c327892e25c9e6ba5a09a732
Carlin Au <carlinau@gmail.com>
2017-05-06 16:44:01 -0400

    Bricks are placeable

---
+++ Assets/Scripts/PlaceBrick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBrick : MonoBehaviour {
+
+    public GameObject Brick;
+    public GameObject RightHand;
+
+	// Use this for initialization
+	void Start () {
+        RightHand = GameObject.Find("Hand - Right");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1))
+        {
+            Instantiate(Brick, RightHand.transform.position, Quaternion.identity);
+        }
+	}
+}
