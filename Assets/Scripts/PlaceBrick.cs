ab9a75fd7be3c14a9cc7db5fef7af36cdebc4032
toniau <toni.au94@gmail.com>
2017-05-08 16:16:59 -0400

    Click and hold to position brick placement

--- Assets/Scripts/PlaceBrick.cs
+++ Assets/Scripts/PlaceBrick.cs
@@ -6,6 +6,8 @@ public class PlaceBrick : MonoBehaviour {
 
     public GameObject Brick;
     public GameObject RightHand;
+	public int MaxBrickCount = 0;
+	private GameObject instance;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +16,28 @@ public class PlaceBrick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (Input.GetMouseButtonDown(1))
         {
-            Instantiate(Brick, RightHand.transform.position, Quaternion.identity);
+            instance = Instantiate(Brick, RightHand.transform.position, Quaternion.identity);
+			instance.name = "brickSpawn";
+			MaxBrickCount += 1;
+			cloneCounter();
         }
+		if (Input.GetMouseButton(1) && instance){
+			instance.transform.position = RightHand.transform.position;
+			instance.transform.position.z = 10;
+		}
+		if (Input.GetMouseButtonUp(1) && instance){
+			instance = null;
+		}
+	}
+	
+	// Keep track of clones
+	void cloneCounter() {
+		if(MaxBrickCount == 15){
+			Destroy(GameObject.Find("brickSpawn"));
+			MaxBrickCount -= 1;
+		}
 	}
 }
